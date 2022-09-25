62c28997b19716039f993da601d1186eccd9ca40
Luis Angel <angelfgdeveloper@gmail.com>
2022-09-25 15:44:38 -0500

    Formularios, Thymeleaf y DataBinding primera parte

--- src/main/java/com/springboot/form/app/springbootform/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/springbootform/controllers/FormController.java
@@ -1,21 +1,93 @@
 package com.springboot.form.app.springbootform.controllers;
 
+// import java.util.HashMap;
+// import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.springboot.form.app.springbootform.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("user") // nombre del objeto que queremos que persista
 public class FormController {
 
   @GetMapping("/form")
   public String form(Model model) {
+    Usuario usuario = new Usuario();
+
+    // Nombres por default
+    usuario.setNombre("Luis");
+    usuario.setApellido("FG");
+    usuario.setIdentificador("123.456.789-K"); // Se pierde al pasar a la vista
+
+    model.addAttribute("titulo", "Formulario de usuario");
+    model.addAttribute("user", usuario); // Evitamos el error en el value del input
     return "form";
   }
 
+  // #2 optimizado
   @PostMapping("/form")
-  public String procesar(Model model) {
+  // BindingResult => Obtiene los errores del primer objeto a validar
+  public String procesar(
+    @Valid @ModelAttribute("user") Usuario usuario, // @ModelAttribute("user") cambiar el nombre de forma automatica
+    BindingResult result, 
+    Model model,
+    SessionStatus status
+  ) {
+    model.addAttribute("titulo", "Resultado del formulario");
+
+    if (result.hasErrors()) { 
+      // #1 errores manuales
+      // Valida si tiene errores
+      // Map<String, String> errores = new HashMap<>(); // Forma manual de obtener errores
+      // result.getFieldErrors().forEach(err -> {
+      //   errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())); // key => nombre del campo | value => mensaje de error
+      // });
+
+      // model.addAttribute("error", errores);
+
+      return "form"; // Regresar a la vista
+    }
+
+    // Objeto completo
+    model.addAttribute("usuario", usuario);
+    status.setComplete(); // completar el proceso y elimina el objeto de la sesion 
+
     return "resultado";
   }
+
+  // #1
+  // @PostMapping("/form")
+  // public String procesar(
+  //   Model model, 
+  //   @RequestParam(name = "username") String username,
+  //   @RequestParam(name = "email") String email,
+  //   @RequestParam(name = "password") String password
+  // ) {
+
+  //   Usuario usuario = new Usuario(); // Entity o clase pojo
+  //   usuario.setUsername(username);
+  //   usuario.setEmail(email);
+  //   usuario.setPassword(password);
+
+  //   // model.addAttribute("username", username);
+  //   // model.addAttribute("email", email);
+  //   // model.addAttribute("password", password);
+
+  //   // Objeto completo
+  //   model.addAttribute("usuario", usuario);
+
+  //   return "resultado";
+  // }
   
 }
---
+++ src/main/java/com/springboot/form/app/springbootform/models/domain/Usuario.java
@@ -0,0 +1,79 @@
+package com.springboot.form.app.springbootform.models.domain;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Usuario {
+
+  private String identificador;
+
+  @NotEmpty(message = "El nombre no puede ser vacio") // Mensaje de error
+  private String nombre;
+
+  @NotEmpty
+  private String apellido;
+
+  @NotEmpty // Tiene que ser distinto de null
+  @Size(min = 3, max = 8) // string largo
+  private String username;
+
+  @NotEmpty
+  @Email(message = "El correo no es válido")
+  private String email;
+
+  @NotEmpty
+  private String password;
+
+  public Usuario() {
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getIdentificador() {
+    return identificador;
+  }
+
+  public void setIdentificador(String identificador) {
+    this.identificador = identificador;
+  }
+
+}
